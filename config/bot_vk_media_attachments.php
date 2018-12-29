788025e8fd406154af8e1b870bbdd4b21b8cb516
Alexey <slivkinalexey20@gmail.com>
2018-12-29 07:48:51 +0300

    code refact

--- config/bot_vk_media_attachments.php
+++ config/bot_vk_media_attachments.php
@@ -1,9 +1,8 @@
 <?php
 return [
-    'faq_buy_attachment'        => 'photo-175591301_456239018',
-    'faq_payment_attachment'    => 'photo-175591301_456239019',
-    'faq_delivery_attachment'   => 'photo-175591301_456239020',
-    'faq_money_back_attachment' => 'photo-175591301_456239021',
-    'about_workers_attachment'  => 'photo-175591301_456239022',
-
+    'faq_buy_attachment'        => 'photo-175591301_456239024',
+    'faq_payment_attachment'    => 'photo-175591301_456239027',
+    'faq_delivery_attachment'   => 'photo-175591301_456239026',
+    'faq_money_back_attachment' => 'photo-175591301_456239028',
+    'about_workers_attachment'  => 'photo-175591301_456239025',
 ];
